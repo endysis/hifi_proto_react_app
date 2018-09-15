e3e71cae93047881fa0158518735df4bd42cb8d8
douglaso <osheadouglas@rentalcars.com>
2018-09-15 16:37:19 +0100

    re added App and css files

--- spotify_hifi/src/App.js
+++ spotify_hifi/src/App.js
@@ -2,17 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let standardTextColor = '#fff';
+let standardStyle = {
+  color: standardTextColor
+}
+
+class Aggregate extends Component {
+  render(){
+    return (
+      <div style={{...standardStyle, width: "40%", display: 'inline-block'}}>
+          <h2 style={{color: standardTextColor}}>Playlist text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={standardStyle}>
+          <img/>
+          <input type="text"/>
+          Filter
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render(){
+    return (
+    <div style = {{...standardStyle, display: 'inline-block',width: "25"}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul><li>Song 0</li><li>Song 1</li><li>Song 2</li></ul>
+      </div>
+    )
+
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{color: standardTextColor}} className="App">
+      <h1>The title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
   }
